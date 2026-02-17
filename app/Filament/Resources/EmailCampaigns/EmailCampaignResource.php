2f4b54ba810732859dc627ae4f3630b9a1992a88
Anas-Shareef <muhammedanaspp098@gmail.com>
2026-02-17 22:23:58 +0530

    Add EmailCampaign resource to Marketing navigation

--- app/Filament/Resources/EmailCampaigns/EmailCampaignResource.php
+++ app/Filament/Resources/EmailCampaigns/EmailCampaignResource.php
@@ -17,10 +17,32 @@ use Filament\Tables\Table;
 class EmailCampaignResource extends Resource
 {
     protected static ?string $model = EmailCampaign::class;
-
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    
+    protected static string|\UnitEnum|null $navigationGroup = 'Marketing';
+    
+    protected static ?int $navigationSort = 2;
 
     protected static ?string $recordTitleAttribute = 'title';
+    
+    // Prevent access until migrations are run
+    public static function canAccess(): bool
+    {
+        try {
+            return \Schema::hasTable('email_campaigns');
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+    
+    public static function canViewAny(): bool
+    {
+        return static::canAccess();
+    }
+    
+    public static function shouldRegisterNavigation(): bool
+    {
+        return static::canAccess();
+    }
 
     public static function form(Schema $schema): Schema
     {
